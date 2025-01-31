13b48a598e3de02209bb3f83952241727ae36175
Vikash Gupta <000000vikash@gmail.com>
2025-01-31 15:21:52 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  public_key = file("/Users/vikash/Documents/work/vikash/terra-practice/terra-key.pub")
 }
 
 resource "aws_default_vpc" "default" {
